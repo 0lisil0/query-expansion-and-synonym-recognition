c10361075ca3396193312b8aba33a37e05513c9d
0lisi0 <lisilei2@illinois.edu>
2024-11-23 19:27:49 -0600

    expand_query

--- backend/query_expand.py
+++ backend/query_expand.py
@@ -1,5 +1,7 @@
-import nltk
 import os
+
+import nltk
+from nltk.corpus import wordnet as wn
 
 ########### TODO: you might need to change the path ###########
 # download wordnet to destination folder
@@ -10,3 +12,18 @@ nltk.download('wordnet', download_dir=venv_nitk_path)
 # you can also just use `nltk.download('wordnet')` without specifying path
 ########### TODO: you might need to change the path ###########
 
+
+def expand_query(query: str) -> list:
+    """Expand the input query string by finding synonyms.
+    Args:
+        - query (str): the user input query string
+    Returns:
+        - list: a list of synonyms
+    """
+    synonyms = []
+
+    return synonyms
+
+
+if __name__ == '__main__':
+    eq = expand_query('software engineer')
