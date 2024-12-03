f85c972ec82ea574af25c57b3baf1a112d0d4011
pyangyu <89769081+pyangyu@users.noreply.github.com>
2024-12-03 17:28:08 -0600

    add flask backend and js html frontend

---
+++ project/app.py
@@ -0,0 +1,33 @@
+from flask import Flask, request, jsonify, send_from_directory
+from backend.query_expand import expand_query
+from flask_cors import CORS
+
+app = Flask(__name__)
+CORS(app) # enable Cross-Origin Resource Sharing, looks like it is not useful tho
+
+# Serve the HTML js: http://127.0.0.1:5000
+@app.route('/')
+def index():
+    return send_from_directory('static', 'index.html')
+
+# Add the `/expand` endpoint to call the def expand_query() function under backend and in query_expand
+@app.route('/expand', methods=['POST'])
+def expand():
+    try:
+        # Parse JSON data
+        data = request.get_json()
+        word = data.get('word')
+
+        if not word:
+            return jsonify({'error': 'No word provided'}), 400
+
+        # Call the `expand_query` function, which is currently a build-in function now
+        # TODO: probably implement our own expand_query function here? but it might be time-consuming~~
+        synonyms = expand_query(word)
+        return jsonify({'synonyms': synonyms})
+    except Exception as e:
+        # internal fault return 500
+        return jsonify({'error': str(e)}), 500
+
+if __name__ == '__main__':
+    app.run(debug=True)
