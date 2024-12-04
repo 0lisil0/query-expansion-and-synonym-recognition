42f69659f80f93b3d9f44d377c3c08a62e4b9f76
ivyqiuhan <he0qiuhan@gmail.com>
2024-12-03 22:18:44 -0800

    added radio button

--- project/app.py
+++ project/app.py
@@ -10,19 +10,44 @@ from nltk.corpus import wordnet as wn
 
 ########### TODO: you might need to change the path ###########
 # download wordnet to destination folder
-root = os.path.dirname(os.path.dirname(__file__))
-venv_nitk_path = os.path.join(root, '.venv', 'nltk_data')
-nltk.download('wordnet', download_dir=venv_nitk_path)
+# root = os.path.dirname(os.path.dirname(__file__))
+# venv_nitk_path = os.path.join(root, '.venv', 'nltk_data')
+# nltk.download('wordnet', download_dir=venv_nitk_path)
 
 # add to path
-nltk.data.path.append(venv_nitk_path)
+# nltk.data.path.append(venv_nitk_path)
 # you can also just use `nltk.download('wordnet')` without specifying path
 ########### TODO: you might need to change the path ###########
+nltk.download('wordnet')
 
 app = Flask(__name__)
 CORS(app)  # enable Cross-Origin Resource Sharing, looks like it is not useful tho
 
 # Serve the HTML js: http://127.0.0.1:5000
+
+class Option1Handler:
+    def process(self, word):
+        print(f"Processed '{word}' with Option 1")
+        return bert_expand_query(word)
+
+class Option2Handler:
+    def process(self, word):
+        print(f"Processed '{word}' with Option 2")
+        synonym_finder = LlamaQuerySynonymFinder(model_name="meta-llama/Llama-Vision-Free")
+        return synonym_finder.get_synonyms(word, num_synonyms=5)
+
+
+class Option3Handler:
+    def process(self, word):
+        print(f"Processed '{word}' with Option 3")
+        return nltk_expand_query(word)
+
+
+option_map = {
+    "Option1": Option1Handler(),
+    "Option2": Option2Handler(),
+    "Option3": Option3Handler(),
+}
 
 
 @app.route('/')
@@ -38,14 +63,21 @@ def expand():
         # Parse JSON data
         data = request.get_json()
         word = data.get('word')
+        option = data.get('option')
 
         if not word:
             return jsonify({'error': 'No word provided'}), 400
 
         # Call the `expand_query` function, which is currently a build-in function now
         # TODO: probably implement our own expand_query function here, but it might be time-consuming
-        synonyms = bert_expand_query(word)
-        return jsonify({'synonyms': synonyms})
+
+        handler = option_map.get(option)
+        if not handler:
+            return jsonify({"error": "Invalid option selected"}), 400
+
+        synonyms = handler.process(word)
+        return jsonify({"synonyms": synonyms}), 200
+
     except Exception as e:
         # internal fault return 500
         return jsonify({'error': str(e)}), 500
