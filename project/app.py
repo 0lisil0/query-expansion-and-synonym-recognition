c1401096d4dda204b22b15ad3e732217a748e891
0lisi0 <lisilei2@illinois.edu>
2024-12-03 22:52:46 -0600

    import all three algorithm

--- project/app.py
+++ project/app.py
@@ -1,16 +1,22 @@
-from flask import Flask, request, jsonify, send_from_directory
-from backend.sentence_bert_query_expand import expand_query
+from backend.llm_query_expansion import LlamaQuerySynonymFinder
+from backend.nltk_query_expand import nltk_expand_query
+from backend.sentence_bert_query_expand import bert_expand_query
+from flask import Flask, jsonify, request, send_from_directory
 from flask_cors import CORS
 
 app = Flask(__name__)
-CORS(app) # enable Cross-Origin Resource Sharing, looks like it is not useful tho
+CORS(app)  # enable Cross-Origin Resource Sharing, looks like it is not useful tho
 
 # Serve the HTML js: http://127.0.0.1:5000
+
+
 @app.route('/')
 def index():
     return send_from_directory('static', 'index.html')
 
 # Add the `/expand` endpoint to call the def expand_query() function under backend and in query_expand
+
+
 @app.route('/expand', methods=['POST'])
 def expand():
     try:
@@ -23,11 +29,12 @@ def expand():
 
         # Call the `expand_query` function, which is currently a build-in function now
         # TODO: probably implement our own expand_query function here, but it might be time-consuming
-        synonyms = expand_query(word)
+        synonyms = bert_expand_query(word)
         return jsonify({'synonyms': synonyms})
     except Exception as e:
         # internal fault return 500
         return jsonify({'error': str(e)}), 500
+
 
 if __name__ == '__main__':
     app.run(debug=True)
